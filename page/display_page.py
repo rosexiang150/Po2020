19b62fd6253be409611998a01a2816534f0a3da9
rosexiang150 <2248011149@qq.com>
2020-03-31 19:19:13 +0800

    初步封装了find_element方法

--- page/display_page.py
+++ page/display_page.py
@@ -1,5 +1,14 @@
+from selenium.webdriver.common.by import By
 
 class DisplayPage:
+
+    search_button = By.ID, 'com.android.settings:id/search'
+    input_text_view = By.ID, 'android:id/search_src_text'
+    back_button = By.CLASS_NAME, 'android.widget.ImageButton'
+
+
+    def find_element(self, loc):
+        return self.driver.find_element(loc[0], loc[1])
 
     def __init__(self, driver):
         self.driver = driver
@@ -7,14 +16,17 @@ class DisplayPage:
 
 
     def click_search(self):
-        ele = self.driver.find_element_by_id('com.android.settings:id/search')
+        ele = self.find_element(self.search_button)
         ele.click()
 
     def input_text(self, text):
-        ele = self.driver.find_element_by_id('android:id/search_src_text')
+        ele = self.find_element(self.input_text_view)
         ele.click()
         ele.clear()
         ele.send_keys(text)
 
     def click_back(self):
-        self.driver.find_element_by_class_name('android.widget.ImageButton').click()
+        self.find_element(self.back_button).click()
+
+
+
--- page/network_page.py
+++ page/network_page.py
@@ -1,26 +1,46 @@
+from selenium.webdriver.common.by import By
 
 class NetworkPage:
+
+    more_button = By.XPATH, '//*[@text="更多"]'
+    network_button = By.XPATH, '//*[@text="移动网络"]'
+    net2G_button = By.XPATH, '//*[@text="2G"]'
+    net3G_button = By.XPATH, '//*[@text="3G"]'
+    firstNet_button = By.ID, 'android:id/title'
+
 
     def __init__(self, driver):
         self.driver = driver
 
     def click_3g(self):
-        self.driver.find_element_by_xpath('//*[@text="更多"]').click()
-        self.driver.find_element_by_xpath('//*[@text="移动网络"]').click()
-        list1 = self.driver.find_elements_by_id('android:id/title')
+        # self.driver.find_element_by_xpath('//*[@text="更多"]').click()
+        self.find_element(self.more_button).click()
+        # self.driver.find_element_by_xpath('//*[@text="移动网络"]').click()
+        self.find_element(self.network_button).click()
+        list1 = self.find_elements(self.firstNet_button)
         for i in list1:
             if i.get_attribute('text') == "首选网络类型":
                 i.click()
                 break
-        self.driver.find_element_by_xpath('//*[@text="3G"]').click()
+        self.find_element(self.net3G_button).click()
 
 
     def click_2g(self):
-        self.driver.find_element_by_xpath('//*[@text="更多"]').click()
-        self.driver.find_element_by_xpath('//*[@text="移动网络"]').click()
-        list1 = self.driver.find_elements_by_id('android:id/title')
+        # self.driver.find_element_by_xpath('//*[@text="更多"]').click()
+        self.find_element(self.more_button).click()
+        # self.driver.find_element_by_xpath('//*[@text="移动网络"]').click()
+        self.find_element(self.network_button).click()
+        # list1 = self.driver.find_elements_by_id('android:id/title')
+        list1 = self.find_elements(self.firstNet_button)
         for i in list1:
             if i.get_attribute('text') == "首选网络类型":
                 i.click()
                 break
-        self.driver.find_element_by_xpath('//*[@text="2G"]').click()
+        # self.driver.find_element_by_xpath('//*[@text="2G"]').click()
+        self.find_element(self.net2G_button).click()
+
+    def find_element(self, loc):
+        return self.driver.find_element(loc[0], loc[1])
+
+    def find_elements(self, loc):
+        return self.driver.find_elements(loc[0], loc[1])
